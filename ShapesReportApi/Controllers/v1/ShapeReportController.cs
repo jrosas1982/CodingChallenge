96a87536a5c19ca150d88d468e5c452472020e15
Juan Pablo <jrosas1982@gmail.com>
2022-09-26 19:10:04 -0300

    Proyecto API que consume del proyecto de aplicacion

---
+++ ShapesReportApi/Controllers/v1/ShapeReportController.cs
@@ -0,0 +1,31 @@
+﻿using CodingChallenge.Data.Classes;
+using Core.Challenge.Application.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShapesReportApi.Controllers.v1
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class ShapeReportController : ControllerBase
+    {
+        private readonly IShapesReportService _shapesReportService;
+        private readonly ILogger<ShapeReportController> _logger;
+
+        public ShapeReportController(ILogger<ShapeReportController> logger, IShapesReportService shapesReportService)
+        {
+            _logger = logger;
+            _shapesReportService = shapesReportService;
+        }
+
+        [HttpGet]
+        public IEnumerable<ShapeBasic> Get()
+        {
+            return _shapesReportService.GetShapes().ToArray();
+        }
+    }
+}
